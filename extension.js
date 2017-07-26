37c3ba611696fa504fa1bf1f11d405902c89bec8
=roberto <roberto.torres.dev@gmail.com>
2017-07-26 03:08:40 -0400

    doing some work.

--- extension.js
+++ extension.js
@@ -1,30 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+"use strict";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let vscode = require('vscode');
+
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "pytest-helper" is now active!');
+    let createScopedFixtureDisposable = vscode.commands.registerCommand('extension.createScopedFixture', () => {
+        const fixtureScopes = vscode.workspace.getConfiguration("pytestHelper")["availableFixtureScopes"];
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showQuickPick(fixtureScopes, {placeHolder: "Select scope for the new fixture"}).then(value => {
+            vscode.window.showInformationMessage(value);
+        });
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(createScopedFixtureDisposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
